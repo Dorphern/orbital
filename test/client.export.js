a49e1ca67703f3d940581af19fa419bd5d524639
Kristoffer Olesen <kristofferdo@gmail.com>
2014-06-22 17:43:28 +0200

    bundle and serve methods to client through browserify

--- test/client.export.js
+++ test/client.export.js
@@ -2,8 +2,8 @@ var http = require('http');
 var orbital = require('../');
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello man \n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<script src="/orbital/orbital.js"></script>\n');
 }).listen(1337, '127.0.0.1');
 
 console.log("started server");
